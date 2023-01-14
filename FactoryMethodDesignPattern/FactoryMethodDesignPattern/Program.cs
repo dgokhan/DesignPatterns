4814bc37e12ad74629b50b712c83b4a3f24b64c4
Gökhan Doğru <info@gokhandogru.net>
2023-01-15 02:01:54 +0300

    Imported information into Program.cs

--- FactoryMethodDesignPattern/FactoryMethodDesignPattern/Program.cs
+++ FactoryMethodDesignPattern/FactoryMethodDesignPattern/Program.cs
@@ -6,4 +6,7 @@ using FactoryMethodDesignPattern.Models.Base;
 CarFactory factory = new CarFactory();
 Car myCar = factory.Create(CarTypes.Sedan);
 
+Console.WriteLine($"Araç teker sayısı: {myCar.Wheel}");
+Console.WriteLine($"Sunroof varmı?: {myCar.HasSunRoof}");
+Console.WriteLine($"Elektrikli mi?: {myCar.IsElectric}");
 Console.ReadLine();
